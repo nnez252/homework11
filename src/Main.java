ca1a4b37624833e9ec93d10e3bae361018ba7bbf
Егор <nnez252@gmail.com>
2022-11-17 17:57:42 +0200

    задание 11

--- src/Main.java
+++ src/Main.java
@@ -11,30 +11,30 @@ public class Main {
         }
         return deliverydays;
     }
-//    public static void printoc() {
-//        int clientDeviceYear = LocalDate.now().getYear();
-//        int clientOs = 1;
-//        if (clientOs == 0 && clientDeviceYear >=2015){
-//            System.out.println("Установите версию приложения для iOS по ссылке");
-//        } else if (clientOs == 0 && clientDeviceYear < 2015) {
-//            System.out.println("Установите упращенную версию приложения для IOS по ссылке");
-//        } if ( clientOs == 1 && clientDeviceYear>=2015){
-//            System.out.println("Установите версию приложения для Android по ссылке");
-//        } else if (clientOs == 1 && clientDeviceYear<2015) {
-//            System.out.println("Установите упращенную версию приложения для iOS по ссылке");
-//        }
-//    }
-//    public static void printGradeYear() {
-//        int year = 2024;
-//        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0){
-//            System.out.println(year+ " - Высокосный");
-//        }else {
-//            System.out.println(year+" - Обычыный");
-//        }
-//    }
+   public static void printoc() {
+        int clientDeviceYear = LocalDate.now().getYear();
+        int clientOs = 1;
+        if (clientOs == 0 && clientDeviceYear >=2015){
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOs == 0 && clientDeviceYear < 2015) {
+            System.out.println("Установите упращенную версию приложения для IOS по ссылке");
+        } if ( clientOs == 1 && clientDeviceYear>=2015){
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else if (clientOs == 1 && clientDeviceYear<2015) {
+            System.out.println("Установите упращенную версию приложения для iOS по ссылке");
+        }
+    }
+    public static void printGradeYear() {
+        int year = 2024;
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0){
+            System.out.println(year+ " - Высокосный");
+        }else {
+            System.out.println(year+" - Обычыный");
+        }
+    }
     public static void main(String[] args) {
-//printGradeYear();
-//  printoc();
+printGradeYear();
+  printoc();
         System.out.println(sum());
         }
     }
